d726caffec246a4d8afe450773923340a4100adb
Chris Gantt <gantt.art@gmail.com>
2021-03-26 13:18:14 -1000

    Commented out previous server code and brought in starter code for lab 10.

--- modules/weather.js
+++ modules/weather.js
@@ -1,29 +1,79 @@
 'use strict';
-const superagent = require('superagent');
 
-function Forecast(dailyForecast) {
-  this.date = dailyForecast.datetime;
-  this.description = dailyForecast.weather.description;
-}
-
-function getWeather(request, response) {
-  const url = 'http://api.weatherbit.io/v2.0/forecast/daily';
-  const query = {
-    key: process.env.WEATHER_API_KEY,
-    lat: request.query.lat,
-    lon: request.query.lon
-  };
-
-  superagent
-    .get(url)
-    .query(query)
-    .then(forecast => {
-      const forecastArray = forecast.body.data.map(day => new Forecast(day));
-      response.status(200).send(forecastArray);
-    })
-    .catch(err => {
-      response.status(500).send(err.message);
-    });
-}
+let cache = require('./cache.js');
 
 module.exports = getWeather;
+
+function getWeather(latitude, longitude) {
+  const key = 'weather-' + latitude + longitude;
+  const url = 'http://api.weatherbit.io/v2.0/forecast/daily';
+  const queryParams = {
+    key: WEATHER_API_KEY,
+    lang: 'en',
+    lat: lat,
+    lon: lon,
+    days: 5,
+  };
+
+  if (cache[key] && (Date.now() - cache[key].timestamp < 50000)) {
+    console.log('Cache hit');
+  } else {
+    console.log('Cache miss');
+    cache[key] = {};
+    cache[key].timestamp = Date.now();
+    cache[key].data = superagent.get(url)
+    .then(response => parseWeather(response.body));
+  }
+  
+  return cache[key].data;
+}
+
+function parseWeather(weatherData) {
+  try {
+    const weatherSummaries = weatherData.data.map(day => {
+      return new Weather(day);
+    });
+    return Promise.resolve(weatherSummaries);
+  } catch (e) {
+    return Promise.reject(e);
+  }
+}
+
+class Weather {
+  constructor(day) {
+    this.forecast = day.weather.description;
+    this.time = day.datetime;
+  }
+}
+
+
+
+// 'use strict';
+// const superagent = require('superagent');
+
+// function Forecast(dailyForecast) {
+//   this.date = dailyForecast.datetime;
+//   this.description = dailyForecast.weather.description;
+// }
+
+// function getWeather(request, response) {
+//   const url = 'http://api.weatherbit.io/v2.0/forecast/daily';
+//   const query = {
+//     key: process.env.WEATHER_API_KEY,
+//     lat: request.query.lat,
+//     lon: request.query.lon
+//   };
+
+//   superagent
+//     .get(url)
+//     .query(query)
+//     .then(forecast => {
+//       const forecastArray = forecast.body.data.map(day => new Forecast(day));
+//       response.status(200).send(forecastArray);
+//     })
+//     .catch(err => {
+//       response.status(500).send(err.message);
+//     });
+// }
+
+// module.exports = getWeather;
--- server.js
+++ server.js
@@ -1,20 +1,48 @@
 'use strict';
 
+require('dotenv');
 const express = require('express');
-require('dotenv').config();
 const cors = require('cors');
+
+const weather = require('./modules/weather.js');
 const app = express();
-app.use(cors());
-const PORT = process.env.PORT;
 
-const getWeather = require('./modules/weather');
-const getMovies = require('./modules/movies');
+app.get('/weather', weatherHandler);
 
-app.get('/weather', getWeather);
-app.get('/movies', getMovies);
+function weatherHandler(request, response) {
+  const { lat, lon } = request.query;
+  weather(lat, lon)
+  .then(summaries => response.send(summaries))
+  .catch((error) => {
+    console.error(error);
+    response.status(200).send('Sorry. Something went wrong!')
+  });
+}  
 
-app.use('*', (request, response) => {
-  response.status(500).send('Server Error');
-});
+app.listen(process.env.PORT, () => console.log(`Server up on ${process.env.PORT}`));
 
-app.listen(PORT, () => console.log(`listening on ${PORT}`));
+
+
+
+
+
+// 'use strict';
+
+// const express = require('express');
+// require('dotenv').config();
+// const cors = require('cors');
+// const app = express();
+// app.use(cors());
+// const PORT = process.env.PORT;
+
+// const getWeather = require('./modules/weather');
+// const getMovies = require('./modules/movies');
+
+// app.get('/weather', getWeather);
+// app.get('/movies', getMovies);
+
+// app.use('*', (request, response) => {
+//   response.status(500).send('Server Error');
+// });
+
+// app.listen(PORT, () => console.log(`listening on ${PORT}`));
